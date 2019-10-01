b90a3f73e4c8f5ef1279ace046a27bb283bda4b7
Luis <canatel@uninorte.edu.co>
2019-10-01 18:59:21 -0500

    Paquete de chat con la interface

--- app/src/main/java/com/example/myfirstapplication/Chat/ChatActivity.java
+++ app/src/main/java/com/example/myfirstapplication/Chat/ChatActivity.java
@@ -3,14 +3,30 @@ package com.example.myfirstapplication.Chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.myfirstapplication.R;
 
+import java.util.Random;
+
 public class ChatActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        // Aqui es donde se escribe el mensaje
+        editText = (EditText) findViewById(R.id.editText);
+    }
+
+    private String getRandomColor() {
+        Random r = new Random();
+        StringBuffer sb = new StringBuffer("#");
+        while(sb.length() < 7){
+            sb.append(Integer.toHexString(r.nextInt()));
+        }
+        return sb.toString().substring(0, 7);
     }
 }
