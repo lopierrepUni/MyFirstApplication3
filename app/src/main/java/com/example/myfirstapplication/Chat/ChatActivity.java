67be173bf72f403cdb228a200c9c7929fcd2cb6a
Luis <canatel@uninorte.edu.co>
2019-10-02 18:29:43 -0500

    Chat pero no renderiza

--- app/src/main/java/com/example/myfirstapplication/Chat/ChatActivity.java
+++ app/src/main/java/com/example/myfirstapplication/Chat/ChatActivity.java
@@ -3,7 +3,9 @@ package com.example.myfirstapplication.Chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.example.myfirstapplication.R;
 
@@ -12,6 +14,10 @@ import java.util.Random;
 public class ChatActivity extends AppCompatActivity {
 
     private EditText editText;
+    ListView lv;
+    Message mensaje;
+    MessageAdapter messageAdapter;
+    MemberData data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,10 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
         // Aqui es donde se escribe el mensaje aqui a
         editText = (EditText) findViewById(R.id.editText);
+        data = new MemberData("usuario",getRandomColor());
+        lv = findViewById(R.id.messages_view);
+        messageAdapter = new MessageAdapter(getApplicationContext());
+        lv.setAdapter(messageAdapter);
     }
 
     private String getRandomColor() {
@@ -28,5 +38,14 @@ public class ChatActivity extends AppCompatActivity {
             sb.append(Integer.toHexString(r.nextInt()));
         }
         return sb.toString().substring(0, 7);
+    }
+
+    public void Send(View view) {
+        String message = editText.getText().toString();
+        mensaje = new Message(message,data,false);
+        if (message.length() > 0){
+            messageAdapter.add(mensaje);
+            editText.getText().clear();
+        }
     }
 }
